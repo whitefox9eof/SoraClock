dbc76d6cd8a16bc677fd2a683e9e38fcc0e718b6
whitefox9 <whitefox9eof@gmail.com>
2020-07-16 14:26:02 +0900

    時刻表示、時報機能追加

--- SoraClock/MainWindow.xaml.cs
+++ SoraClock/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System;
+using System.Media;
+using System.Windows;
+using System.Windows.Threading;
 
 namespace SoraClock
 {
@@ -7,9 +10,97 @@ namespace SoraClock
     /// </summary>
     public partial class MainWindow : Window
     {
+        private MainSettings settings;
+        private DispatcherTimer timer;
+        private int currentHour;
+        private SoundPlayer player;
+
         public MainWindow()
         {
             InitializeComponent();
+            // タイマーの初期設定
+            timer = new DispatcherTimer(DispatcherPriority.SystemIdle);
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += Timer_Tick;
+            currentHour = DateTime.Now.Hour;
+            timer.Start();
+            // ウィンドウの初期設定
+            settings = MainSettings.Default;
+            Topmost = settings.Topmost;
+            if (settings.WindowLeft >= 0 &&
+                (settings.WindowLeft + Width) < SystemParameters.VirtualScreenWidth)
+            { Left = settings.WindowLeft; }
+            else
+            { Left = (SystemParameters.VirtualScreenWidth - Width) / 2; }
+            if (settings.WindowTop >= 0 &&
+                (settings.WindowTop + Height) < SystemParameters.VirtualScreenHeight)
+            { Top = settings.WindowTop; }
+            else
+            { Top = (SystemParameters.VirtualScreenHeight - Height) / 2; }
+        }
+
+        /// <summary>
+        /// 時刻更新イベント
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            DateTime nowDateTime = DateTime.Now;
+            clockTextBlock.Text = nowDateTime.ToString("HH:mm");
+            if(nowDateTime.Hour != currentHour)
+            {
+                player = new SoundPlayer("resources/voice/time-" + nowDateTime.Hour + ".wav");
+                player.Play();
+                currentHour = nowDateTime.Hour;
+            }
+        }
+
+        /// <summary>
+        /// コンテキストメニュー終了
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void exitMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            // 終了前にウィンドウ位置を保存
+            settings.WindowLeft = Left;
+            settings.WindowTop = Top;
+            settings.Save();
+            Application.Current.Shutdown();
+        }
+
+        /// <summary>
+        /// 最前面表示
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void topmostMenuItem_Checked(object sender, RoutedEventArgs e)
+        {
+            Topmost = true;
+            settings.Topmost = true;
+            settings.Save();
+        }
+        /// <summary>
+        /// 最前面解除
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void topmostMenuItem_Unchecked(object sender, RoutedEventArgs e)
+        {
+            Topmost = false;
+            settings.Topmost = false;
+            settings.Save();
+        }
+
+        /// <summary>
+        /// ドラッグでウィンドウ移動
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            this.DragMove();
         }
     }
 }
